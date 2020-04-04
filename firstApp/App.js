326278b88056c1d27cb858408c60a2b8b5b1ccdc
jawad727 <jawaddhussein@gmail.com>
2020-04-03 20:52:12 -0700

    basic todo app working

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,41 +1,46 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from "./components/Header"
+import TodoItem from "./components/TodoItem"
+import AddTodo from "./components/AddTodo"
 
 export default function App() {
 
-  const [people, setPeople] = useState([
-    { name: "shaun", id: "1" },
-    { name: "john", id: "2" },
-    { name: "crystal", id: "3" },
-    { name: "chance", id: "4" },
-    { name: "timmy", id: "5" },
-    { name: "guy", id: "6" },
-    { name: "fieri", id: "7" },
-    { name: "ghandi", id: "8" }
+  const [todos, setTodos] = useState([
+   { text: "buy coffee", key: 1 },
+   { text: "create an app", key: 2 },
+   { text: "play on the switch", key: 3 }
   ])
 
-   const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => { // prevPeople (or any arg in set___ hook refers to the old state)
-      return prevPeople.filter(item => item.id != id)
-    });
+  const pressHandler = (key) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.key != key)
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos]
+    })
   }
 
   return (
     <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        <AddTodo submitHandler={submitHandler} />
+        <View style={styles.list}>
 
-    <FlatList // FlatList is more efficient than ScrollView because  it doesn't automatically render everything, but rather waits until you've scrolled to render
-      numColumns={2} // (optional) 
-      keyExtractor={(item) => item.id} //allows us to have IDs or other attributes as keys 
-      data={people}
-      renderItem={( {item} ) => (
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
+          />
 
-        <TouchableOpacity onPress={() => { // TouchableOpacity allows items to be clicked
-          pressHandler(item.id)}}>
-          <Text style={styles.item} > {item.name} </Text>
-        </TouchableOpacity>
+        </View>
 
-    )} />
+      </View>
       
     </View>
   );
@@ -44,18 +49,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff'
   },
-  item : {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    margin: 10,
+  content: {
+    padding: 40,
+    // marginLeft: 10
   }
+
 
 });
