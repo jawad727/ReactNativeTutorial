624e7f4d36d89cf4db0cb9dabe72e4bbe0f2d4cc
jawad727 <jawaddhussein@gmail.com>
2020-04-03 17:45:40 -0700

    FlatList array rendering (more effecient than ScrollView)

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 
 export default function App() {
 
   const [people, setPeople] = useState([
-    { name: "shaun", key: "1" },
-    { name: "john", key: "2" },
-    { name: "crystal", key: "3" },
-    { name: "chance", key: "4" },
-    { name: "timmy", key: "5" }
+    { name: "shaun", id: "1" },
+    { name: "john", id: "2" },
+    { name: "crystal", id: "3" },
+    { name: "chance", id: "4" },
+    { name: "timmy", id: "5" },
+    { name: "guy", id: "6" },
+    { name: "fieri", id: "7" },
+    { name: "ghandi", id: "8" }
   ])
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {people.map(item => ( 
 
+    <FlatList // FlatList is more efficient than ScrollView because  it doesnt automatically render everything, but rather waits until you've scrolled to render
+      numColumns={1}
+      keyExtractor={(item) => item.id} //allows us to have IDs or other attributes as keys
+      data={people}
+      renderItem={( {item} ) => (
+        <Text style={styles.item} > {item.name} </Text>
+    )} />
+      
+      {/* <ScrollView>
+        {people.map(item => ( 
             <View key={item.key}>
               <Text style={styles.item} > {item.name} </Text>
             </View>
-
         ))}
-      </ScrollView>
+      </ScrollView> */}
     </View>
   );
 }
